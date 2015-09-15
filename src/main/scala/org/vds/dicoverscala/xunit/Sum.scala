8576e2800156ade0df7ce176de11e77f70c7a47c
Dawid Świst <dawid.swist@gmail.com>
2015-09-15 22:01:34 +0200

    - Add support for scalaTest  - Add support for inteliJ

---
+++ src/main/scala/org/vds/dicoverscala/xunit/Sum.scala
@@ -0,0 +1,17 @@
+package org.vds.dicoverscala.xunit
+
+
+/**
+ * Example of object. Object can sum
+ */
+object Sum {
+  /**
+   * Sum two number
+   * @param a fist number
+   * @param b second number
+   * @return result of sum a and b
+   */
+  def add(a: Int, b: Int): Int = {
+    return a + b;
+  }
+}
---
+++ src/test/scala/org/vds/dicoverscala/xunit/Sum$TestWithFunSuite.scala
@@ -0,0 +1,38 @@
+package org.vds.dicoverscala.xunit
+
+import org.scalatest.{WordSpec, FlatSpec, FunSuite}
+
+
+class Sum$TestWithFunSuite extends FunSuite {
+  test("should Sum.add(1,3) be equal 3") {
+
+    val a = 1 + 2
+
+    assert(Sum.add(1, 2) == 3)
+  }
+}
+
+class Sum$TestWithFlatSuite extends FlatSpec {
+
+  "Sum.add(1,3)" should "be equal 3" in {
+    assert(Sum.add(1, 2) == 3)
+  }
+
+  "Sum.add(0)" should "be equals 0" in {
+    assertResult(0) {
+      Sum.add(0, 0)
+    }
+  }
+}
+
+class Sum$TestWithWordSpec extends WordSpec {
+  "Sum" when {
+    "add(1,2)" should {
+      "be equals 3" in {
+        assert(Sum.add(1, 2) == 3)
+      }
+    }
+  }
+
+
+}
